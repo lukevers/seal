9fd18b03d50b251d99c1184c8f6fdae078ebb2fb
Luke Evers <luke@lukevers.com>
2018-11-15 22:38:00 -0500

    Stylize settings

--- gui/app/src/modules/content/components/settings.js
+++ gui/app/src/modules/content/components/settings.js
@@ -1,7 +1,8 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import { themes } from '../../../base/themes';
 
 import {
     fetchSettingsIfNeeded,
@@ -50,32 +51,78 @@ class Settings extends Component {
         const setting = this.getSettings();
 
         return (
-            <div>
+            <div css={css`
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+
+                .form-group {
+                    margin: .5em 0;
+                    border-bottom: 1px solid ${themes.standard.lighter.primary};
+                    width: 300px;
+                    display: flex;
+
+                    label {
+                        padding: .5em 0;
+                    }
+
+                    input {
+                        padding: .5em 1em;
+                        border: 0;
+                        width: 100%;
+                    }
+
+                    input:focus{
+                        outline: none;
+                    }
+                }
+
+                button {
+                    width: 300px;
+                    border: 0;
+                    padding: 1em;
+                    background-color: ${themes.standard.primary};
+                    color: ${themes.standard.white};
+                    cursor: pointer;
+
+                    &:hover {
+                        background-color: ${themes.standard.darker.primary};
+                    }
+                }
+            `}>
+                <div class="form-group">
+                    <label for="url">URL</label>
+                    <input
+                        type="text"
+                        name="url"
+                        value={setting.url}
+                        onChange={(e) => this.handleChange(e.target.value, 'url')}
+                    />
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        value={setting.email}
+                        onChange={(e) => this.handleChange(e.target.value, 'email')}
+                    />
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={setting.password}
+                        onChange={(e) => this.handleChange(e.target.value, 'password')}
+                    />
+                </div>
+
                 <button onClick={this.saveSettings}>Save</button>
-
-                url:
-                <input
-                    type="text"
-                    name="url"
-                    value={setting.url}
-                    onChange={(e) => this.handleChange(e.target.value, 'url')}
-                />
-
-                email:
-                <input
-                    type="text"
-                    name="email"
-                    value={setting.email}
-                    onChange={(e) => this.handleChange(e.target.value, 'email')}
-                />
-
-                password:
-                <input
-                    type="password"
-                    name="password"
-                    value={setting.password}
-                    onChange={(e) => this.handleChange(e.target.value, 'password')}
-                />
             </div>
         );
     }
