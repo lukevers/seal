4c1e4f6ba7dd3d51f4eee2f9f817bb137d88020b
Luke Evers <luke@lukevers.com>
2019-01-22 23:16:31 -0500

    Make subscriber emails unique per email/team_id combo

--- server/migrations/3_create_subscribers_table.up.sql
+++ server/migrations/3_create_subscribers_table.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE `subscribers` (
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` timestamp NULL DEFAULT NULL,
+    UNIQUE KEY `subscribers_email_team_id` (`email`,`team_id`),
     KEY `subscribers_team_id_foreign_key` (`team_id`),
     CONSTRAINT `subscribers_team_id_foreign_key` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE RESTRICT,
     PRIMARY KEY (`id`)
